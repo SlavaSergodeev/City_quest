c6f63421f59d7a3984e073cbfde72d1b19f18a09
Александр Уваров < >
2016-12-04 16:42:57 +1000

    изменение модели

--- city_quest/quest/models.py
+++ city_quest/quest/models.py
@@ -4,22 +4,22 @@ from django.utils import timezone
 # Create your models here.
 
 class Quest(models.Model):
-    name=models.CharField(max_length=30,verbose_name="Название квеста")
-    description=models.TextField( )
-    task=models.TextField()
-    maps=models.TextField()
+    name=models.CharField(max_length=30, verbose_name="Название квеста")
+    description=models.TextField(verbose_name="Описание квеста")
+    task=models.TextField(verbose_name="Задание квеста")
+    maps=models.TextField(verbose_name="Карта")
     create_date=models.DateTimeField(default=timezone.now())
     implementation=models.BooleanField(default=False)
-    start_date=models.DateTimeField()
-    end_date=models.DateTimeField()
-    check=models.CharField(max_length=30)
+    start_date=models.DateTimeField(verbose_name="Дата начала квеста")
+    end_date=models.DateTimeField(verbose_name="Дата окончаниея квеста")
+    check=models.CharField(max_length=30, verbose_name="Слово проверки прохождения квеста")
     def __str__(self):
         return self.name
 
 class Photo(models.Model):
     questID=models.ForeignKey('Quest')
-    name=models.CharField(max_length=50)
-    text=models.CharField(max_length=60)
-    date=models.DateTimeField()
+    name=models.CharField(max_length=50, verbose_name="Название фото")
+    text=models.CharField(max_length=60, verbose_name="Текст")
+    date=models.DateTimeField(verbose_name="Дата")
     def __str__(self):
         return self.text
